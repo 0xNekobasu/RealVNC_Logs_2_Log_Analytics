23d0ae1e67cc1db3020619e81adcc11df505d6e5
Oskar Grainger <oskar@theglounge.uk>
2026-01-25 21:19:56 +0000

    Setup project to its basic state

--- Portal_Logs_2_Sentinel/rvncPortalLogs.py
+++ Portal_Logs_2_Sentinel/rvncPortalLogs.py
@@ -1,1 +1,32 @@
-#rvncPortalLogs.py
+#!/usr/bin/env python3
+"""
+rvncPortalLogs.py
+
+Calls RealVNC Portal API to pull logs and then pushes them to Microsoft Sentinel SIEM
+
+Usage (typically in crontab or other automation system e.g Jenkins):
+    python3 rvncPortalLogs.py
+
+Notes:
+  - insert note here
+"""
+# Standard Libraries
+import os
+import logging
+
+#Third Party Libraries
+import requests
+from dotenv import load_dotenv
+
+
+#load_dotenv()
+#key= os.getenv("SECRET_KEY")
+#print(key)
+
+
+
+
+def __Main__():
+    print("do something")
+### Run script. 
+__Main__()
